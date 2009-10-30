7bc30361b6cba9f2fa18fe1290b14b5053193299
Berend van Berkum <berend@dotmpe.com>
2009-10-30 20:53:14 +0100

    local @ sam

--- dotmpe/du/ext/__init__.py
+++ dotmpe/du/ext/__init__.py
@@ -1,12 +1,15 @@
-"(hacked) extensions for docutils reStructuredText"
+"""
+Extensions for docutils reStructuredText
+"""
+from docutils.parsers.rst import directives
+
 
 "Add left_margin/right_margin directives"
-from docutils.parsers.rst import directives
 from dotmpe.du.ext.margin import Margin
-directives.register_directive('margin', Margin)
+
 directives.register_directive('margin', Margin)
 
-"Add margin support, override html writer"
+#"Add margin support, override html writer"
 #import dotmpe.du.ext.xhtmlwriter
 #import docutils.writers
 #docutils.writers._writer_aliases['html'] = \
--- dotmpe/du/ext/margin.py
+++ dotmpe/du/ext/margin.py
@@ -15,20 +15,21 @@ class right_margin(nodes.Decorative, nodes.Element): pass
 def side(argument):
     if argument and argument not in ('left', 'right'):
         raise ValueError('side argument must be "left" or "right", not: %s\n' % argument)
-    return argument        
+    return argument
 
+# Override docutils.node.decoration
 class decoration(nodes.decoration):
-    
+
     """
     The decoration node starts with a header and ends with a footer.
-    In between may be the two margins.
+    In between may be two margins, one left and one right.
     """
 
     def get_left_margin(self):
         for child in self.children:
             if isinstance(child, left_margin):
                 return child
-        node = left_margin()                
+        node = left_margin()
         self.append(node)
         return node
 
@@ -36,7 +37,7 @@ class decoration(nodes.decoration):
         for child in self.children:
             if isinstance(child, right_margin):
                 return child
-        node = right_margin()                
+        node = right_margin()
         self.append(node)
         return node
 
@@ -47,7 +48,7 @@ class Margin(Directive):
 
     """
     A decorative element for contents in the side margins of a page.
-    Like the header and footer decorative elements, this allows page 
+    Like the header and footer decorative elements, this allows page
     contents beside the documents body.
 
     A page can have two margings, left and right, multiple occurrences are
@@ -73,12 +74,11 @@ class Margin(Directive):
         text = '\n'.join(self.content)
         if side_value == 'left':
             node = self.state_machine.document.get_decoration().get_left_margin()
-        elif side_value == 'right':            
+        elif side_value == 'right':
             node = self.state_machine.document.get_decoration().get_right_margin()
         node['classes'] += class_value
         if self.content:
             self.state.nested_parse(self.content, self.content_offset, node)
         return []
-
 
 
