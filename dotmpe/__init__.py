2e7762a82b472d8d3b91e6f3ef104c2ce28c4d2f
Berend van Berkum <berend@dotmpe.com>
2011-01-12 20:02:38 +0100

    Added namespace modularity for package, but need to test/extend for submodules.

--- dotmpe/__init__.py
+++ dotmpe/__init__.py
@@ -1,3 +1,23 @@
+
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+# don't prevent use of paste if pkg_resources isn't installed
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
+
+try:
+    import modulefinder
+except ImportError:
+    pass
+else:
+    for p in __path__:
+        modulefinder.AddPackagePath(__name__, p)
+    del p        
+
+
+
 import dotmpe.du.ext
 import dotmpe.du.ext.reader
 import dotmpe.du.ext.writer
