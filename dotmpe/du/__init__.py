5573aab4da5a28b4bbffa1c21c8a95f05c3b6a2f
Berend van Berkum <berend@dotmpe.com>
2011-01-12 20:03:14 +0100

    Testing for Du version now, set to latest version.

---
+++ dotmpe/du/__init__.py
@@ -0,0 +1,24 @@
+
+#try:
+#    import pkg_resources
+#    pkg_resources.declare_namespace(__name__)
+#except ImportError:
+## don't prevent use of paste if pkg_resources isn't installed
+#    from pkgutil import extend_path
+#    __path__ = extend_path(__path__, __name__)
+#
+#try:
+#    import modulefinder
+#except ImportError:
+#    pass
+#else:
+#    for p in __path__:
+#        modulefinder.AddPackagePath(__name__, p)
+#    del p
+
+
+import docutils
+
+du_version = map(int,docutils.__version__.split('.'))
+if (du_version[0] != 0 or (du_version[0] == 0 and du_version[1] != 8)):
+    raise Exception("Unkown version: "+docutils.__version__)
