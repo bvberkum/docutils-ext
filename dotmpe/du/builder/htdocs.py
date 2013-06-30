70837e54d7f41c7df91e385d94ca8a4237c7d78a
B. van Berkum <dev@dotmpe.com>
2013-06-30 13:17:16 +0200

    Removed tabs for spaces.

--- dotmpe/du/builder/htdocs.py
+++ dotmpe/du/builder/htdocs.py
@@ -5,26 +5,26 @@ from dotmpe.du.util import addClass
 
 class Builder(builder.Builder):
 
-	settings_defaults_overrides = {
-			}
+    settings_defaults_overrides = {
+            }
 
-	extractor_spec = [
-				('dotmpe.du.ext.extractor.htdocs', ),
-			]
+    extractor_spec = [
+            ('dotmpe.du.ext.extractor.htdocs', ),
+            ]
 
-	#  xxx: rename to extractor params?
-	store_params = {
-#				'dotmpe.du.ext.extractor.htdocs.Storage': ''
-			}
+    #  xxx: rename to extractor params?
+    store_params = {
+            #'dotmpe.du.ext.extractor.htdocs.Storage': ''
+            }
 
-	class Reader(mpe.Reader):
+    class Reader(mpe.Reader):
 
-		add_class = [
-				'document[0]/section[0],htdocs'
-				]
+        add_class = [
+                'document[0]/section[0],htdocs'
+                ]
 
-		def get_transforms(self):
-			return mpe.Reader.get_transforms(self) + [
-					addClass(Builder.Reader.add_class) ]
+        def get_transforms(self):
+            return mpe.Reader.get_transforms(self) + [
+                    addClass(Builder.Reader.add_class) ]
 
 
