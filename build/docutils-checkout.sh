d1c3d93dbdb1c822144c856835b10e4887bfb359
B. van Berkum <dev@dotmpe.com>
2015-10-11 07:03:44 +0200

    -

--- build/docutils-checkout.sh
+++ build/docutils-checkout.sh
@@ -1,7 +1,7 @@
 test -e $HOME/src/python-docutils/docutils-svn || {
     mkdir -vp $HOME/src/python-docutils
     #svn checkout http://svn.berlios.de/svnroot/repos/docutils \
-    svn checkout http://svn.code.sf.net/p/docutils/code/trunk \
+    svn checkout http://svn.code.sf.net/p/docutils/code \
         $HOME/src/python-docutils/docutils-svn
     ln -s docutils-svn $HOME/src/python-docutils/working
 }
