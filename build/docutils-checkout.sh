c0b36def21c20a8da30aa639b7ca84a1291a73f4
B. van Berkum <dev@dotmpe.com>
2015-10-11 07:14:06 +0200

    -

--- build/docutils-checkout.sh
+++ build/docutils-checkout.sh
@@ -1,7 +1,8 @@
 test -e $HOME/src/python-docutils/docutils-svn || {
-    mkdir -vp $HOME/src/python-docutils
-    #svn checkout http://svn.berlios.de/svnroot/repos/docutils \
-    svn checkout -q http://svn.code.sf.net/p/docutils/code \
-        $HOME/src/python-docutils/docutils-svn
-    ln -s docutils-svn $HOME/src/python-docutils/working
+    mkdir -vp $HOME/src/python-docutils/docutils-svn/branches
+    #svn checkout http://svn.berlios.de/svnroot/repos/docutils
+    svn checkout -q http://svn.code.sf.net/p/docutils/code/branches/lossless-rst-writer \
+        $HOME/src/python-docutils/docutils-svn/branches/lossless-rst-writer
+
+    ln -s docutils-svn $HOME/src/python-docutils/latest
 }
