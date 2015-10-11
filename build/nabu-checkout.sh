f59c8bf91ec13a2ebaca4628f19a22929f0c67d1
B. van Berkum <dev@dotmpe.com>
2015-10-11 05:25:03 +0200

    -

--- build/nabu-checkout.sh
+++ build/nabu-checkout.sh
@@ -1,6 +1,6 @@
 
-mkdir -vp $HOME/src/nabu
 test -d $HOME/src/nabu/nabu-hg || {
+    mkdir -vp $HOME/src/nabu
     hg clone https://bitbucket.org/blais/nabu $HOME/src/nabu/nabu-hg
     ln -s nabu-hg $HOME/src/nabu/working
 }
--- build/rebuild-lib-links.sh
+++ build/rebuild-lib-links.sh
@@ -1,10 +1,13 @@
 
 test -n "$1" || set -- "/src"
 
-ln -s /src/aafigure lib/aafigure
-ln -s /src/python-docutils/docutils/docutils/docutils lib/docutils
-ln -s /src/python-docutils/latest/branches lib/docutils-branches
-ln -s /src/python-docutils/latest/branches/lossless-rst-writer/docutils lib/docutils-lossless
-ln -s /src/python-docutils/latest/trunk/sandbox lib/docutils-sandbox
-ln -s /src/nabu/working/lib/python/nabu lib/nabu
+mkdir -vp lib
+rm -rf lib/*
+
+ln -s $1/aafigure lib/aafigure
+ln -s $1/python-docutils/docutils/docutils/docutils lib/docutils
+ln -s $1/python-docutils/latest/branches lib/docutils-branches
+ln -s $1/python-docutils/latest/branches/lossless-rst-writer/docutils lib/docutils-lossless
+ln -s $1/python-docutils/latest/trunk/sandbox lib/docutils-sandbox
+ln -s $1/nabu/working/lib/python/nabu lib/nabu
 
