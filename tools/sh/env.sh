81db055c29a796f42c65c28e1055cf24f418d1b1
B. van Berkum <dev@dotmpe.com>
2018-02-12 03:25:16 +0100

    Accum

--- tools/sh/env.sh
+++ tools/sh/env.sh
@@ -1,18 +1,16 @@
 
-set -e
-
 # Easiest way I see to add dev checkout to env
 export PYTHONPATH=.:$PYTHONPATH:test
 
 export PATH=tools:$PATH
-
 
 # Load virtualenv (should be initialized by htd run init)
 test ! -d ~/.pyvenv/du-ext || {
 
   test ! -x "$(which htd)" || {
     htd ispyvenv || {
-        source ~/.pyvenv/du-ext/bin/activate;
+      source ~/.pyvenv/du-ext/bin/activate ||
+        echo "Error activating venv 'du-ext' ($?)" >&2
     }
   }
 }
